e2f8cb8f4cee8ad2197a513407cbf9fe61c83be9
ertangunes <35131284+ertangunes@users.noreply.github.com>
2025-11-17 21:22:18 +0400

    Update check.js

--- pages/api/check.js
+++ pages/api/check.js
@@ -23,7 +23,7 @@ Format:
 `;
 // Vercel'i yeni anahtari kullanmaya zorla
 const response = await fetch(
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent",
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent",
 // ...
       {
         method: "POST",
